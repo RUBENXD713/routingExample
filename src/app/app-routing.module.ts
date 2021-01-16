05d9b776d6fc54d9bbe6e6c826cefd1b4d6918f9
RUBENXD713 <71050267+RUBENXD713@users.noreply.github.com>
2021-01-15 22:38:43 -0600

    primerEjemplo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first-component', component: FirstComponent },
+  { path: 'second-component', component: SecondComponent },
+  { path: '**', component: NotFoundComponent},
+  { path: ' ',   redirectTo: '/first-component', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
